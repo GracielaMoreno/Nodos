efcc94541b22c156385e769320b74ea751e96bc2
GracielaMoreno <chelinesmoreno@hotmail.com>
2017-01-13 12:13:27 -0500

    Update Aplicacion.java

--- Aplicacion.java
+++ Aplicacion.java
@@ -22,35 +22,10 @@ public class Aplicacion {
 		/////obtener el dato
 		System.out.println("\nObtener el dato del nodo en la posicion 3 ");
 		System.out.println(lista.getDato(3));
-		////insertar en un siguiente
-		System.out.println("\nInserta un nodo con dato 16 despues del 15");
-		lista.insrtarPorReferencia(15, 16);
-		lista.listar();
-		System.out.print(" Tamaño: ");
-		System.out.println(lista.getTam());
 
-		System.out.println("\nInsrta un nodo con dato 44 en la posición 3");
-		lista.insrtarPorPosicion(3, 44);
-		lista.listar();
-		System.out.print(" Tmaño: ");
-		System.out.println(lista.getTam());
-		/////cambiar popr nodo
-		System.out.println("\nCambiar el dato 12 del tercer nodo por 13");
-		lista.editarPorReferencia(12, 13);
-		lista.listar();
-		////cambiar por posicion
-		System.out.println("\nCambiar el dato nodo en la posición 0 por 17");
-		lista.editarPorPosicion(0, 17);
-		lista.listar();
 		/////elimina por el dayo
 		System.out.println("\nElimina el nodo con el dato 41");
 		lista.removerPorReferencia(41);        
-		lista.listar();
-		System.out.print("  Tamaño: ");
-		System.out.println(lista.getTam());
-		//////elimina por posicion
-		System.out.println("\nElimina el nodo en la posición 4");
-		lista.removerPorPos(4);        
 		lista.listar();
 		System.out.print("  Tamaño: ");
 		System.out.println(lista.getTam());
@@ -66,6 +41,6 @@ public class Aplicacion {
 		////consulta si ta vacia
 		System.out.println("\nConsulta si la lista está vacia");
 		System.out.println(lista.esVacia());
-
+		System.out.println("fin de programa");
 	}  
 }
