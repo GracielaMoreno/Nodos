c32561c55d9bdc649d4d1ea691f2c64cced54ba3
GracielaMoreno <chelinesmoreno@hotmail.com>
2017-01-13 11:51:55 -0500

    Update Aplicacion.java

--- Aplicacion.java
+++ Aplicacion.java
@@ -4,73 +4,68 @@ public class Aplicacion {
 
 	public static void main(String[] args) throws Exception {
 		Lista lista = new Lista();
-System.out.println("<<-- Ejemplo de lista simple -->>\n");
-        
-      
-        lista.agregarAlFinal(12);
-        lista.agregarAlFinal(15);
-        lista.agregarAlFinal(9);
-      
-        lista.agregarAlInicio(41);
-        lista.agregarAlInicio(6);
-        
-        System.out.println("<<-- Lista -->>");
-        lista.listar();
-        
-        System.out.println("\n\n<<-- TamaÒo -->");
-        System.out.println(lista.getTamanio());
-        
-        System.out.println("\n<<-- Obtener el valor del nodo en la posicion 3 -->>");
-        System.out.println(lista.getValor(3));
-        
-        System.out.println("\nInsrta un nodo con valor 16 despues del 15");
-        lista.insertarPorReferencia(15, 16);
-        lista.listar();
-        System.out.print(" | TamaÒo: ");
-        System.out.println(lista.getTamanio());
-        
-        System.out.println("\n\nInsrta un nodo con valor 44 en la posiciÛn 3");
-        lista.insrtarPorPosicion(3, 44);
-        lista.listar();
-        System.out.print(" | TamaÒo: ");
-        System.out.println(lista.getTamanio());
-        
-        System.out.println("\nActualiza el valor 12 del tercer nodo por 13");
-        lista.editarPorReferencia(12, 13);
-        lista.listar();
-        System.out.print(" | TamaÒo: ");
-        System.out.println(lista.getTamanio());
-        
-        System.out.println("\nActualiza el valor nodo en la posiciÛn 0 por 17");
-        lista.editarPorPosicion(0, 17);
-        lista.listar();
-        System.out.print(" | TamaÒo: ");
-        System.out.println(lista.getTamanio());
-        
-        System.out.println("\nElimina el nodo con el valor 41");
-        lista.removerPorReferencia(41);        
-        lista.listar();
-        System.out.print(" | TamaÒo: ");
-        System.out.println(lista.getTamanio());
-        
-        System.out.println("\nElimina el nodo en la posiciÛn 4");
-        lista.removerPorPosicion(4);        
-        lista.listar();
-        System.out.print(" | TamaÒo: ");
-        System.out.println(lista.getTamanio());
-        
-        System.out.println("\nConsulta si existe el valor 30");
-        System.out.println(lista.buscar(30));
-        
-        System.out.println("\nConsulta la posicion del valor 9");
-        System.out.println(lista.getPosicion(9));
-        
-        System.out.println("\nElimina la lista");
-        lista.eliminar();
-        
-        System.out.println("\nConsulta si la lista est· vacia");
-        System.out.println(lista.esVacia());
-        
-        System.out.println("\n\n<<-- Fin de ejemplo lista simple -->>");
-    }  
+		System.out.println("LISTA ENLAZADAS \n");
+
+
+		lista.agregarAlInicio(6);
+
+		lista.agregarAlFinal(9);
+		lista.agregarAlFinal(12);
+		lista.agregarAlInicio(41);
+		lista.agregarAlFinal(15);
+
+		System.out.println(" Lista ");
+		lista.listar();
+		/////tama√±o
+		System.out.println("\n\n Tama√±o ");
+		System.out.println(lista.getTam());
+		/////obtener el dato
+		System.out.println("\nObtener el dato del nodo en la posicion 3 ");
+		System.out.println(lista.getDato(3));
+		////insertar en un siguiente
+		System.out.println("\nInserta un nodo con dato 16 despues del 15");
+		lista.insrtarPorReferencia(15, 16);
+		lista.listar();
+		System.out.print(" Tama√±o: ");
+		System.out.println(lista.getTam());
+
+		System.out.println("\nInsrta un nodo con dato 44 en la posici√≥n 3");
+		lista.insrtarPorPosicion(3, 44);
+		lista.listar();
+		System.out.print(" Tma√±o: ");
+		System.out.println(lista.getTam());
+		/////cambiar popr nodo
+		System.out.println("\nCambiar el dato 12 del tercer nodo por 13");
+		lista.editarPorReferencia(12, 13);
+		lista.listar();
+		////cambiar por posicion
+		System.out.println("\nCambiar el dato nodo en la posici√≥n 0 por 17");
+		lista.editarPorPosicion(0, 17);
+		lista.listar();
+		/////elimina por el dayo
+		System.out.println("\nElimina el nodo con el dato 41");
+		lista.removerPorReferencia(41);        
+		lista.listar();
+		System.out.print("  Tama√±o: ");
+		System.out.println(lista.getTam());
+		//////elimina por posicion
+		System.out.println("\nElimina el nodo en la posici√≥n 4");
+		lista.removerPorPos(4);        
+		lista.listar();
+		System.out.print("  Tama√±o: ");
+		System.out.println(lista.getTam());
+		///consultar
+		System.out.println("\nConsulta si existe el dato 30");
+		System.out.println(lista.buscar(30));
+
+		System.out.println("\nConsulta la posicion del dato 9");
+		System.out.println(lista.getPosicion(9));
+		////eliminar la lista
+		System.out.println("\nEliminar lissta");
+		lista.eliminar();
+		////consulta si ta vacia
+		System.out.println("\nConsulta si la lista est√° vacia");
+		System.out.println(lista.esVacia());
+
+	}  
 }
